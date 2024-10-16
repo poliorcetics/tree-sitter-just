c357f5882fcf8ef7f3e7c32ec640a64af6875bb4
Alexis (Poliorcetics) Bourget <ab_contribs@poliorcetiq.eu>
2024-10-16 22:24:50 +0200

    chore: format flake.nix

--- flake.nix
+++ flake.nix
@@ -5,13 +5,16 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        devShells.default = with pkgs;
+        devShells.default =
+          with pkgs;
           let
             darwinInclude = lib.optionalString stdenv.isDarwin ''- "-I${darwin.Libsystem}/include/"'';
           in
@@ -30,5 +33,6 @@
               EOF
             '';
           };
-      });
+      }
+    );
 }
